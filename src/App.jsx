7b7a1a759ba63706666097232d898a8d823d40bf
Tarique-dataEngineer <tariqueisrar7834@gmail.com>
2025-01-17 16:57:25 +0530

    rebuild

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
-import About from './pages/about';
+import About from './pages/About';
 import ProjectsPage from './pages/ProjectsPage';
 import ContactPage from './pages/ContactPage';
 
