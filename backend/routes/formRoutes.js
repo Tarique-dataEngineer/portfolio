295a464149775eb8e9ac0f39bc07d4d478690917
Tarique-dataEngineer <tariqueisrar7834@gmail.com>
2025-01-19 00:20:23 +0530

    backend seprately

--- backend/routes/formRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { form } from "../controllers/formController.js";
-
-const router = express.Router();
-
-// POST route for form submission
-router.post("/contact", form);
-
-export default router;
